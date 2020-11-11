486dbc11022c8f7115759433d51d58a556624198
Branden Sherman <bshermandeveloper@gmail.com>
2020-11-11 17:09:06 -0500

    Prepare for deployment

--- server/index.js
+++ server/index.js
@@ -52,9 +52,19 @@ app.use('/api', require('./api'));
 // Custom error handler
 app.use(errorHandler);
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  // in production set the frontend build to a static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
